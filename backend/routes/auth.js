bf231c5da6a10232b3ab5126e80418a8fad06780
Wesley Silva <contatowesley7963@gmail.com>
2025-08-06 15:05:26 -0300

    feat: create register route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,17 +7,44 @@ import { API_SECRET } from '../config/index.js';
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body
+
+  if (!username, !password){
+    return res.status(400).json({ message: 'Usuário e senha são obrigatórios' })
+  }
+
+  const existingUser = users.find(u => u.username === username)
+  if (existingUser) {
+    return res.status(409).json({ message: 'Nome de usuário já existente' })
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10)
+    users.push({ username, password: hashedPassword })
+    
+    return res.status(201).json({ message: 'Usuário registrado com sucesso'})
+  } catch(err) {
+    return res.status(500).json({ message: 'Erro ao registrar o usuário'})
+  }
+})
+
+router.post('/login', async (req, res) => {
   const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Usuário e senha são obrigatórios.' });
+  }
+
   const user = users.find(u => u.username === username);
 
   if (!user) {
-    return res.status(401).json({ message: 'Usuário não encontrado' });
+    return res.status(401).json({ message: 'Credenciais inválidas' });
   }
 
-  const passwordMatch = bcrypt.compareSync(password, user.password);
+  const passwordMatch = await bcrypt.compare(password, user.password);
   if (!passwordMatch) {
-    return res.status(401).json({ message: 'Senha incorreta' });
+    return res.status(401).json({ message: 'Credenciais inválidas' });
   }
 
   const token = jwt.sign({ username: user.username }, API_SECRET, { expiresIn: '1h' });
